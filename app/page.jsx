c0e81221ef0712e479dd67c7732f4f4b1dbc6219
Noah <noaligpitan26@gmail.com>
2023-12-18 14:50:44 +0800

    removed wrong imports

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import FullBreedCarousel from "./components/FullBreedCarousel";
+
 import Footer from "./components/Footer";
-import CardSection from "./components/CardSection";
+
 import CardsContainer from "./components/CardsContainer";
 
 export default function Home() {

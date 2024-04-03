5727865d417eb8d53f7376a25135d3d675cc9976
noah <noaligpitan26@gmail.com>
2024-04-03 16:28:32 +0800

    remove localstorage error

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={`${inter.className}`}>{children}</body>
     </html>
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-"use client";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
@@ -10,30 +9,12 @@ import ServicesCards from "./components/ServicesCards";
 import Carousel from "./components/Carousel";
 import Slider from "./components/Slider";
 
-import { useState, useEffect } from "react";
 import DarkModeToggle from "./components/DarkModeToggle";
 
 export default function Home() {
-  const [theme, setTheme] = useState(
-    localStorage?.getItem("theme") ? localStorage?.getItem("theme") : "light"
-  );
-
-  const toggleTheme = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-
-  useEffect(() => {
-    localStorage?.setItem("theme", theme);
-    const localTheme = localStorage.getItem("theme");
-    document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme]);
   return (
     <div className="min-h-screen">
-      <Navbar toggleTheme={toggleTheme} />
+      <Navbar />
 
       <Hero />
       <ServicesCards />
